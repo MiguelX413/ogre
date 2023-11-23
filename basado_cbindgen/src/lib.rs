0b234a5bc91feb8e99b5d5b1b28cd56eab7e518a
MiguelX413 <lbpgaming27@gmail.com>
2023-11-23 03:01:45 -0800

    Add ValidatedStrRef

--- basado_cbindgen/src/lib.rs
+++ basado_cbindgen/src/lib.rs
@@ -42,3 +42,33 @@ impl<'a> From<tokenizer::ParseTokenError<'a>> for ParseTokenError {
         }
     }
 }
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+pub struct ValidatedStrRef<'a> {
+    s: &'a str,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct Utf8Error {
+    utf8error: core::str::Utf8Error,
+}
+
+impl From<core::str::Utf8Error> for Utf8Error {
+    fn from(utf8error: std::str::Utf8Error) -> Self {
+        Self { utf8error }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn validate_str<'a>(str_ref: StrRef) -> Result<ValidatedStrRef<'a>, Utf8Error> {
+    fn inner<'a>(str_ref: StrRef) -> std::result::Result<ValidatedStrRef<'a>, std::str::Utf8Error> {
+        Ok(ValidatedStrRef {
+            s: std::str::from_utf8(unsafe {
+                std::slice::from_raw_parts(str_ref.ptr, str_ref.len)
+            })?,
+        })
+    }
+    inner(str_ref).map_err(Utf8Error::from).into()
+}
