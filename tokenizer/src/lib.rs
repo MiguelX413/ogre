7fe383f99cbfca931c3d10a198ea2a6c946744df
MiguelX413 <lbpgaming27@gmail.com>
2023-11-23 00:04:07 -0800

    Split project into library and demo crates

--- tokenizer/src/lib.rs
+++ tokenizer/src/lib.rs
@@ -312,31 +312,3 @@ impl<'a> Iterator for SplitTokens<'a> {
 pub fn split_tokens(string: &str) -> SplitTokens {
     SplitTokens::new(string)
 }
-
-pub fn main() {
-    [
-        "catfood-45",
-        "catfood",
-        "67z23",
-        "catfood&-45",
-        "&",
-        " -45 - 45 + +45",
-        "if +2 + -2 else x := x - 5 ",
-        "if {{10 / {45 + 3}} + {2 * 4}} - +5",
-        "日本語a+123",
-        "cat- 324_32432432432-ref",
-        "{2133 ** 21} % 2",
-        r#"let my_string := "lol\"test";
-let xd: Int := 2;
-let multi_line_str := "xd\
-sus";"#,
-        "let _ := 5;",
-    ]
-    .into_iter()
-    .for_each(|string| {
-        println!(
-            "{string:?}: {:?}",
-            split_tokens(string).collect::<Result<Vec<_>, _>>()
-        )
-    });
-}
